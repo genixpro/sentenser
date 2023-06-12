1b9ff9677e22342f9b5a958f1b5af9396f9cfd8b
Bradley Arsenault <bradley@checksum.ai>
2023-06-12 09:53:12 -0700

    Added in additional prompts and text styles to the extension.

--- background.js
+++ background.js
@@ -1,4 +1,63 @@
 const openAIAPIKey = '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';
+const promptEnding = "Try to preserve the main point of what is being said. Rewrite the following:\n";
+const prompts = [
+  {
+    "id": "nice",
+    "title": "Nice and Friendly",
+    "prompt": `Rewrite the following text to be nice and friendly. ` +
+      `Remove any inappropriate content and try to make it professional and respectful. ` +
+      promptEnding,
+  },
+  {
+    "id": "bold",
+    "title": "Bold and Ambitious",
+    "prompt": `Rewrite the following text to be bolder and more ambitious. ` +
+      `Make it sound grand and amazing. ` +
+      promptEnding,
+  },
+  {
+    "id": "happy",
+    "title": "Happy and Optimistic",
+    "prompt": `Rewrite the following text to be happier and more optimistic. ` +
+      `It should put me in a positive mood. ` +
+      `Use positive words and a cheerful tone. ` +
+      promptEnding,
+  },
+  {
+    "id": "logical",
+    "title": "Terse and Logical",
+    "prompt": `Rewrite the following text to be terse and logical. ` +
+      `It should be straight to the point. ` +
+      `Get rid of the fluff. ` +
+      `Remove any and all unnecessary words. ` +
+      promptEnding,
+  },
+  {
+    "id": "funny",
+    "title": "Funny and Humorous",
+    "prompt": `Rewrite the following text to be humorous and funny. ` +
+      `Add in a joke here or there. ` +
+      `Make it more light hearted and fun. ` +
+      promptEnding,
+  },
+  {
+    "id": "official",
+    "title": "Official and Bureaucratic",
+    "prompt": `Rewrite the following text to sound more official and bureaucratic. ` +
+      `It should sound like a robot wrote it, or a very astute communications person. ` +
+      `Make it sound like how a government, large corporation or lawyer would write a press release. ` +
+      promptEnding,
+  },
+]
+
+function findPromptById(id) {
+  for (let prompt of prompts) {
+    if (prompt.id === id) {
+      return prompt;
+    }
+  }
+  return null;
+}
 
 function replaceSelectedText(newText, tab) {
   // Send a message to the content script to replace the selected text
@@ -10,9 +69,9 @@ function replaceSelectedText(newText, tab) {
 
 
 function handleClick(info, tab) {
-  console.log("Selected text:", info.selectionText);
-
-  // replaceSelectedText("Hello, world!", tab);
+  const prompt = findPromptById(info.menuItemId);
+  const selectedText = info.selectionText;
+  const chatGptCommand = `${prompt.prompt}\n${selectedText}`;
 
   // Make an API call using the fetch API
   fetch("https://api.openai.com/v1/chat/completions", {
@@ -23,7 +82,7 @@ function handleClick(info, tab) {
     },
     body: JSON.stringify({
       "model": "gpt-3.5-turbo",
-      "messages": [{"role": "user", "content": `Rewrite the following text to be nicer. Remove any inappropriate content, but try to preserve the main thrust of what is being said:\n\n${info.selectionText}`}],
+      "messages": [{"role": "user", "content": chatGptCommand}],
       "temperature": 0.7
     }), // Pass any data in the request body
   })
@@ -31,18 +90,27 @@ function handleClick(info, tab) {
     .then((data) => {
       const text = data.choices[0].message.content;
       replaceSelectedText(text, tab);
-      // replaceSelectedText(JSON.stringify(data, null, 4), tab);
     })
     .catch((error) => {
       replaceSelectedText(JSON.stringify(error, null, 4), tab);
     });
 }
 
+
 browser.contextMenus.create({
-  id: "rewrite-nicer-text",
-  title: "Rewrite text to be nicer",
+  id: "rewrite-text-base",
+  title: "Rewrite text",
   contexts: ["selection"],
 });
+
+for (let promptInfo of prompts) {
+  browser.contextMenus.create({
+    id: promptInfo.id,
+    parentId: "rewrite-text-base",
+    title: promptInfo.title,
+    contexts: ["selection"],
+  });
+}
 
 browser.contextMenus.onClicked.addListener(handleClick);
 
